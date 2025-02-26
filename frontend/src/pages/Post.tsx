80a6690857ea8b3fd9de50f0bb9a2703bd28373b
Sneiden Fernandes <sneiden@Sneidens-MacBook-Pro-4.local>
2025-02-26 21:08:11 +0000

    sdf

--- frontend/src/pages/Post.tsx
+++ frontend/src/pages/Post.tsx
@@ -16,14 +16,14 @@ export function Post(){
             </div>
 
 
-            <div className="px-[10vw] md:px-[25vw] lg:px-[30vw] mt-5 bg-slate-50">
+            <div className="px-[10vw] md:px-[25vw] lg:px-[30vw]  sm:mt-5 bg-slate-50">
 
             <FullEntryCard username={location.state.user} datePosted={location.state.date} text={location.state.text} wordOfTheDay={location.state.word}/>
 
             </div>
             <div className="flex justify-center mt-[5vh]">
 
-            <button className="border-[1px] bg-black ml-[2vw] text-sm md:text-md  text-black text-slate-50 border-black rounded-2xl px-4 py-1 flex" onClick={()=>{
+            <button className="border-[1px] bg-black text-sm md:text-md  text-black text-slate-50 border-black rounded-2xl px-4 py-1 flex" onClick={()=>{
                 navigate(-1)
             }}>
                 {"Go Back"}
